58f7c283e0dec0861f5180c80750b714f6b40403
FyuuYT <104697868+FyuuYT@users.noreply.github.com>
2022-10-08 18:57:52 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -10,17 +10,17 @@ global.Api = {
 global.ApiKey = 'coderevolution' //JIKA INGIN MENDAPATKAN KOMISI DAFTAR DI HTTPS://AMPIBISMM.MY.ID UNTUK MENDAPATKAN APIKEY!
 
 global.smm = {
-  nomor : '6283102650464',
-  atasnama : 'Ahmad Nur Fadhil'
+  nomor : '6285845969519',
+  atasnama : 'Fyuuu'
 }
 
 // Other
-global.owner = ['6283102650464',"6282135724604"]
-global.premium = ['6283102650464']
+global.owner = ['6285845969519',"6285845969519"]
+global.premium = ['6285845969519']
 global.sponsor = 'no sponsors in:('
-global.packname = 'Crizzy-Bot'
-global.footer = '© 2022 Fadhil Graphy'
-global.author = 'Owner : Fadhil Graphy\nIg1 : @fdhlgrphy\nIg2 : @nr.fdhl__'
+global.packname = 'Fyuu-bot'
+global.footer = '© Fyuu'
+global.author = 'Owner : Fyuu\nIg1 : @Fyuu_Store'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.act = {
 		wait: 'please wait a moment, processing data...',
